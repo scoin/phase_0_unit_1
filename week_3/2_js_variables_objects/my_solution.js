fad46c4f9b267ba22b74c732b43c580300859790
scoin <greg.piccolo@gmail.com>
2014-04-16 13:16:37 -0400

    work completed

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,40 +1,41 @@
-// I paired [by myself, with:] on this challenge.
-
-
-
+// I was by myself on this challenge.
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// 1 & 2. set secretNumber equal to 7
+// 3 & 4. set password equal to "just open the door"
+// 5 & 6. set allowedIn equal to false
+// 7, 8 & 9 create an array members containing "John" as the first element and "Mary" as the 4th
 
 
 // __________________________________________
 // Write your code below.
-
-
-
-
-
-
+var secretNumber = 7;
+var password = 'just open the door'
+var allowedIn = false;
+var members = ["John", null, null, "Mary"];
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
-
-
-
-
-
+/* 
+I believe my code is efficient as it can possibly be. As it is only setting variables, and I am using
+the variables outlined in the driver code, they are clear and concise. The code does not repeat
+itself, it seeks to solve multiple tests with each line. It also does not waste memory in the 
+array, but setting nulls instead of arbitrary strings.
+*/
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
+/*
+At first, I admit I had no idea what I was doing. I overthought it.
+As soon as I slowed down and realized why this file was structured as it is, and that I was 
+merely setting variables, I read through the tests again and solved them. 
+Because they went from general to specific I could solve more than one
+test by writing just a single line. This strategy worked well and did not require me to refactor.
+I initially did not understand that the functions parameters were being set on different lines.
+That kind of threw me off, until I remembered from the js intro that this was how it was done so
+long as there is a comma at the end of line.
+I enjoyed the satsifaction of completing the tests and watching them ring true, 2 - 3 at a time.
 
+*/
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
