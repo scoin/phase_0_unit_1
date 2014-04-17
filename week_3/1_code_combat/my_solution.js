7c94aa3e17c26efb5c0878d946c060b202ea0346
scoin <greg.piccolo@gmail.com>
2014-04-17 14:51:50 -0400

    work completed

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,4 +1,4 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
